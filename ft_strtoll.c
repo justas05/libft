c193ddf005e75add34d7e130a92868c8fc48e553
justas05 <power-makar@rambler.ru>
2020-11-13 16:52:40 +0300

    add strtoll, but not implement

--- ft_strtoll.c
+++ ft_strtoll.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_strtoll.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbooke <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -11,8 +11,62 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
-int		ft_atoi(const char *buf)
+static void		ft_skip_spaces(char **buf)
 {
-	return ((int)ft_strtoll(buf));
+	while (ft_isspace(**buf))
+		++(*buf);
+}
+
+static long		ft_get_sign(char **buf)
+{
+	long sign;
+
+	sign = 1;
+	if (**buf == '-' || **buf == '+')
+	{
+		if (**buf == '-')
+			sign = -1;
+		++(*buf);
+	}
+	return (sign);
+}
+
+static long		ft_get_base(char **buf)
+{
+	long base;
+
+	base = 10;
+	if (**buf == '0')
+	{
+		base = 8;
+		++(*buf);
+		if (**buf == 'x' || **buf == 'X')
+		{
+			base = 16;
+			++(*buf);
+		}
+	}
+	return (base);
+}
+
+long			ft_strtol(const char *buf)
+{
+	long res;
+	long sign;
+	long base;
+
+	res = 0;
+	sign = 1;
+	ft_skip_spaces(&buf);
+	sign = ft_get_sign(&buf);
+	base = ft_get_base(&buf);
+	while (*buf && ft_isdigit(*buf))
+	{
+		res *= 10;
+		res += *buf - '0';
+		++buf;
+	}
+	return (res * sign);
 }
